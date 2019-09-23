136bfd0f454233f031319df15b8d0d3bba672f50
jtertil <jtertil@gmail.com>
2019-09-23 13:13:37 +0200

    basic @pytest.fixture example

--- case1/test_phonebook.py
+++ case1/test_phonebook.py
@@ -3,20 +3,22 @@ import pytest
 from case1.phonebook import Phonebook
 
 
-def test_lookup_by_name():
-    phonebook = Phonebook()
+@pytest.fixture
+def phonebook():
+    return Phonebook()
+
+
+def test_lookup_by_name(phonebook):
     phonebook.add("Bob", "1234")
     assert "1234" == phonebook.lookup("Bob")
 
 
-def test_contain_all_names():
-    phonebook = Phonebook()
+def test_contain_all_names(phonebook):
     phonebook.add("Bob", "1234")
     assert "Bob" in phonebook.get_names()
 
 
-def test_missing_name_raises_error():
-    phonebook = Phonebook()
+def test_missing_name_raises_error(phonebook):
     with pytest.raises(KeyError):
         phonebook.lookup("Missing")
 
